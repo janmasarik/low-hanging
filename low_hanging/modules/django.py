e932c5e2c327f532b22886767be85ca26e0b447a
Jan Masarik <cambion9999@gmail.com>
2019-01-19 00:33:37 +0100

    Add references to modules

--- low_hanging/modules/django.py
+++ low_hanging/modules/django.py
@@ -3,6 +3,7 @@ from low_hanging.base_worker import BaseWorker
 
 class DjangoDebug(BaseWorker):
     name = "django_debug"
+    references = ["https://stackoverflow.com/questions/14470601/debug-true-django"]
     def run(self):
         path = self.random_string()
         r = self.scrape(path)
--- low_hanging/modules/phpinfo.py
+++ low_hanging/modules/phpinfo.py
@@ -3,6 +3,7 @@ from low_hanging.base_worker import BaseWorker
 
 class PhpInfo(BaseWorker):
     name = "phpinfo_exposed"
+    references = ["https://hackerone.com/reports/165930"]
     def run(self):
         paths = ["phpinfo.php", "info.php"]  # remove info.php for lower false positives
         for path in paths:
